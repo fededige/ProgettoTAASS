46608c8643cb2a323f2520ce18bcae5831e70831
Federico <fededigeronimo@gmail.com>
2023-11-25 17:54:19 +0100

    started input-book component

---
+++ ui/src/app/input-book/input-book.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input-book',
+  templateUrl: './input-book.component.html',
+  styleUrl: './input-book.component.css'
+})
+export class InputBookComponent {
+
+}
