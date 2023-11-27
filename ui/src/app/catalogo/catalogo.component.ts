e55734d31aa3d27b521c0a0d3b54cd43991abe68
Luca Bonamico <luba2001.lb21@gmail.com>
2023-11-27 12:16:24 +0100

    create catalog page

--- ui/src/app/catalogo/catalogo.component.ts
+++ ui/src/app/catalogo/catalogo.component.ts
@@ -7,4 +7,46 @@ import { Component } from '@angular/core';
 })
 export class CatalogoComponent {
 
+  /* DATI CHE ARRIVARENNO DAL DATABASE */
+  libriTendenza = [
+    {
+      "title": "1984",
+      "author": "George Orwell",
+      "editions": 27,
+      "cover": "https://th.bing.com/th/id/OIP.gX0ExEtzFf0AkuD2xmX2iAHaMF?rs=1&pid=ImgDetMain"
+    },
+    {
+      "title": "1984",
+      "author": "George Orwell",
+      "editions": 27,
+      "cover": "https://th.bing.com/th/id/OIP.gX0ExEtzFf0AkuD2xmX2iAHaMF?rs=1&pid=ImgDetMain"
+    },
+    {
+      "title": "1984",
+      "author": "George Orwell",
+      "editions": 27,
+      "cover": "https://th.bing.com/th/id/OIP.gX0ExEtzFf0AkuD2xmX2iAHaMF?rs=1&pid=ImgDetMain"
+    }
+  ];
+
+  libriPiuLetti = [
+    {
+      "title": "Storia di una ladra di libri",
+      "author": "Markus Zusak",
+      "editions": 10,
+      "cover": "https://www.sperling.it/content/uploads/2015/11/978886836304HIG.JPG"
+    }, 
+    {
+      "title": "Storia di una ladra di libri",
+      "author": "Markus Zusak",
+      "editions": 10,
+      "cover": "https://www.sperling.it/content/uploads/2015/11/978886836304HIG.JPG"
+    }, 
+    {
+      "title": "Storia di una ladra di libri",
+      "author": "Markus Zusak",
+      "editions": 10,
+      "cover": "https://www.sperling.it/content/uploads/2015/11/978886836304HIG.JPG"
+    }
+  ]
 }
