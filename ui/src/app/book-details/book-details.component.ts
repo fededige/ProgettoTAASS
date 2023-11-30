8c5b073922e474754fd017cea65acb22b907680a
Luca Bonamico <luba2001.lb21@gmail.com>
2023-11-30 17:07:14 +0100

    book details component

--- ui/src/app/book-details/book-details.component.ts
+++ ui/src/app/book-details/book-details.component.ts
@@ -8,11 +8,26 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class BookDetailsComponent implements OnInit{
   title: any
+  book_details!: any
 
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.title = this.route.snapshot.paramMap.get('title')
+    // book = getBookDetails(title) ---> prendere dati del libro da visualizzare dal db
     
+    // simulazione chiamata al db
+    this.book_details = {
+      "title": this.title,
+      "author": "George Orwell",
+      "cover": "https://th.bing.com/th/id/OIP.gX0ExEtzFf0AkuD2xmX2iAHaMF?rs=1&pid=ImgDetMain",
+      "owner": "Francesco",
+      "price": 4,
+      "plot": "‘ 1984 ‘ by George Orwell is a dystopian novel set in a totalitarian society led by the omnipresent Big Brother. The story follows Winston Smith, who works for the Ministry of Truth, altering historical records. Discontent with the regime, Winston begins a forbidden relationship with Julia.",
+      "condition": "excellent",
+      "edition": "1984",
+      "restitution": 30,
+      "evaluation": 4.5
+    }
   }
 }
